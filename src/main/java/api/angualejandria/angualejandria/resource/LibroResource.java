6ffe7d3f9440f2f725c959eedfe2e0196fd10170
jcguerrero21 <jotaceguerrerom21@gmail.com>
2017-11-13 00:02:42 +0100

    UPDATED - Se añade el mappeo y el controlador para añadir la imagen de la portada de los libros (se guardaran en nuestra carpeta resources del programa) si no la tienes creala con la ruta que pone el controlador de LibroResource de SubirImagenPortada

--- src/main/java/api/angualejandria/angualejandria/resource/LibroResource.java
+++ src/main/java/api/angualejandria/angualejandria/resource/LibroResource.java
@@ -3,20 +3,55 @@ package api.angualejandria.angualejandria.resource;
 import api.angualejandria.angualejandria.domain.Libro;
 import api.angualejandria.angualejandria.service.LibroService;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.web.bind.annotation.RequestBody;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RequestMethod;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
+import org.springframework.web.multipart.MultipartFile;
+import org.springframework.web.multipart.MultipartHttpServletRequest;
+
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+import java.awt.print.Book;
+import java.io.BufferedOutputStream;
+import java.io.File;
+import java.io.FileOutputStream;
+import java.util.Iterator;
 
 @RestController
+@RequestMapping("/libro")
 public class LibroResource {
 
     @Autowired
     private LibroService libroService;
 
-    @RequestMapping (value="/libro/añadir", method = RequestMethod.POST)
+    @RequestMapping(value = "/aniadir", method = RequestMethod.POST)
     public Libro añadirLibroPost(@RequestBody Libro libro) {
         return libroService.guardar(libro);
+    }
+
+    @RequestMapping(value="/aniadir/image", method=RequestMethod.POST)
+    public ResponseEntity subirImagenPortadoLibro(
+            @RequestParam("id") Long id,
+            HttpServletResponse response, HttpServletRequest request
+    ){
+        try {
+            Libro libro = libroService.getUno(id);
+            MultipartHttpServletRequest multipartRequest = (MultipartHttpServletRequest) request;
+            Iterator<String> it = multipartRequest.getFileNames();
+            MultipartFile multipartFile = multipartRequest.getFile(it.next());
+            String fileName = "libro_"+id+".png";
+
+
+            byte[] bytes = multipartFile.getBytes();
+            BufferedOutputStream stream = new BufferedOutputStream(new FileOutputStream(new File("src/main/resources/static/image/libro/"+fileName)));
+            stream.write(bytes);
+            stream.close();
+
+            return new ResponseEntity("Upload Success!", HttpStatus.OK);
+        } catch (Exception e) {
+            e.printStackTrace();
+            return new ResponseEntity("Upload failed!", HttpStatus.BAD_REQUEST);
+        }
     }
 
 }
